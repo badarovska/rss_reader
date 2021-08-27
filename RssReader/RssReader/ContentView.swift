1a8209c575d90c6deef38729d92ad0004ba9dd82
Gordana Badarovska <gordana.badarovska@webfactory.mk>
2021-08-27 16:36:37 +0200

    Adds XCode project

---
+++ RssReader/RssReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RssReader
+//
+//  Created by WF | Gordana Badarovska on 27.8.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
